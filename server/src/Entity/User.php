10fab14fea28a5d538adcec90a943d8c314127e5
Alexey Gorshkov <moonhorn33@gmail.com>
2020-08-06 01:16:21 +0300

    Add user entity.

---
+++ server/src/Entity/User.php
@@ -0,0 +1,117 @@
+<?php declare(strict_types=1);
+/**
+ * @author Alexey Gorshkov <moonhorn33@gmail.com>
+ */
+
+namespace App\Entity;
+
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+use League\OAuth2\Server\Entities\UserEntityInterface;
+
+/**
+ * User entity
+ *
+ * @ORM\Entity
+ */
+class User implements UserEntityInterface
+{
+    /**
+     * @var string
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column
+     */
+    private $password;
+
+    /**
+     * @var \DateTimeImmutable
+     *
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new DateTimeImmutable();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getIdentifier(): string
+    {
+        return (string)$this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username Username
+     *
+     * @return User
+     */
+    public function setUsername(string $username): User
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password Password
+     *
+     * @return User
+     */
+    public function setPassword(string $password): User
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTimeImmutable
+     */
+    public function getCreatedAt(): DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+}
--- server/src/Repository/UserRepository.php
+++ server/src/Repository/UserRepository.php
@@ -5,16 +5,34 @@
 
 namespace App\Repository;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use League\OAuth2\Server\Entities\ClientEntityInterface;
 use League\OAuth2\Server\Repositories\UserRepositoryInterface;
 
+/**
+ * User repository
+ */
 class UserRepository implements UserRepositoryInterface
 {
     /**
+     * @var \Doctrine\ORM\EntityRepository
+     */
+    private $repository;
+
+    /**
+     * @param \Doctrine\ORM\EntityManagerInterface $entityManager Entity manager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->repository = $entityManager->getRepository(User::class);
+    }
+
+    /**
      * @inheritDoc
      */
-    public function getUserEntityByUserCredentials($username, $password, $grantType, ClientEntityInterface $clientEntity)
+    public function getUserEntityByUserCredentials($username, $password, $grantType, ClientEntityInterface $clientEntity): ?User
     {
-        // TODO: Implement getUserEntityByUserCredentials() method.
+        return $this->repository->findOneBy(['username' => $username, 'password' => $password]);
     }
 }
